b6498d1bba1f1e0e46d830f8d786315388b63ed3
stefan <stefanace26@gmail.com>
2021-10-30 10:09:49 -0400

    HTML + CSS to React

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(s[ac]ss|css)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
